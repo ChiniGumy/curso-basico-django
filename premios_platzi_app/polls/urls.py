07a488ea404ebc11994de5ff1e01edad590e4f67
David Sánchez Romero <dsebastiansr@hotmail.com>
2023-03-08 11:24:02 -0500

    [CLASS: Creando vistas para la aplicacion]

--- premios_platzi_app/polls/urls.py
+++ premios_platzi_app/polls/urls.py
@@ -2,5 +2,16 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name="index")
+    path('', views.index, name="index"),
+    #ex: /polls/
+
+    path('<int:question_id>/', views.detail, name="index"),
+    #ex: /polls/5/
+
+    path('<int:question_id>/results', views.results, name="index"),
+    #ex: /polls/5/results
+
+    path('<int:question_id>/votes', views.votes, name="index")
+    #ex: /polls/5/votes
+
 ]
--- premios_platzi_app/polls/views.py
+++ premios_platzi_app/polls/views.py
@@ -2,4 +2,16 @@ from django.shortcuts import render
 from django.http import	HttpResponse
 
 def index(request):
-	return HttpResponse('Ola mundo')
+	return HttpResponse('Estas en la pagina principal de Premios Platzi App')
+
+
+def detail(request, question_id):	# Detalle de una pregunta
+	return HttpResponse(f'Estas viendo la pregunta numero {question_id}')
+
+
+def results(request, question_id):	# Cantidad de votos de una pregunta
+	return HttpResponse(f'Estas viendo los resultados de la pregunta numero {question_id}')
+
+
+def votes(request, question_id):
+	return HttpResponse(f'Estas votando a la pregunta numero {question_id}')
