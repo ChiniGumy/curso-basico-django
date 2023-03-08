18097cbef19dfefcd32b3de5eb69619df19c97e5
David Sánchez Romero <dsebastiansr@hotmail.com>
2023-03-08 12:10:04 -0500

    [CLASS: Creando template de Home]

--- premios_platzi_app/polls/views.py
+++ premios_platzi_app/polls/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
 from django.http import	HttpResponse
+from .models import Question 
 
 def index(request):
-	return HttpResponse('Estas en la pagina principal de Premios Platzi App')
+	latest_question_list = Question.objects.all()
+	return render(request, 'polls/index.html', {
+		'latest_question_list': latest_question_list
+	})
 
 
 def detail(request, question_id):	# Detalle de una pregunta
