6a998e657c5275c99f0baaa6abe4f79dbde2763b
David Sánchez Romero <dsebastiansr@hotmail.com>
2023-03-08 14:43:42 -0500

    [CLASS: Utilizando la Etiqueta url para Evitar el Hard Coding]

--- premios_platzi_app/polls/urls.py
+++ premios_platzi_app/polls/urls.py
@@ -1,17 +1,19 @@
 from django.urls import path
 from . import views
 
+app_name = 'polls'
+
 urlpatterns = [
     path('', views.index, name="index"),
     #ex: /polls/
 
-    path('<int:question_id>/', views.detail, name="index"),
+    path('<int:question_id>/detail/sdasdasd/aaaa/aaaaa', views.detail, name="detail"),
     #ex: /polls/5/
 
-    path('<int:question_id>/results', views.results, name="index"),
+    path('<int:question_id>/results', views.results, name="results"),
     #ex: /polls/5/results
 
-    path('<int:question_id>/votes', views.votes, name="index")
+    path('<int:question_id>/votes', views.votes, name="votes")
     #ex: /polls/5/votes
 
 ]
