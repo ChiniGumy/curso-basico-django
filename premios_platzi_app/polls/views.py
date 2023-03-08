57077a8c1addccce335d479d250f56fa7044239f
David Sánchez Romero <dsebastiansr@hotmail.com>
2023-03-08 12:19:02 -0500

    [CLASS: Elevando el error 404]

--- premios_platzi_app/polls/views.py
+++ premios_platzi_app/polls/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import	HttpResponse
 from .models import Question 
 
@@ -10,7 +10,11 @@ def index(request):
 
 
 def detail(request, question_id):	# Detalle de una pregunta
-	return HttpResponse(f'Estas viendo la pregunta numero {question_id}')
+
+	question = get_object_or_404(Question, pk=question_id)
+	return render(request, 'polls/detail.html', {
+		'question': question
+	})
 
 
 def results(request, question_id):	# Cantidad de votos de una pregunta
